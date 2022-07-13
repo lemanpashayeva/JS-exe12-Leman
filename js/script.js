34ca521c83be4a7661bab77314b9a2e702df6ba0
lemanpashayeva <lmnpasayeva@gmail.com>
2022-07-13 16:11:19 +0400

    third commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let ply1 = Math.floor(Math.random() * 10);
+let ply2 = Math.floor(Math.random() * 10);
+
+function Click1(){
+    document.getElementById("screen1").innerText= ply1;
+}
+
+function Click2(){
+    document.getElementById("screen2").innerText= ply2;
+}
+
+function Winner(){
+    if (ply1 > ply2){
+        document.getElementById("winner").innerText=("The WINNER is Player1!")
+    } else if (ply2 >ply1){
+        document.getElementById("winner").innerText=("The WINNER is Player2!")
+    } else {
+        document.getElementById("winner").innerText=("The game ended in a draw!")
+    }
+}
