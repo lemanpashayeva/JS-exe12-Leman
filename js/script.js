98454497df8a2d969096cc37a7ba639be0ce4c82
lemanpashayeva <lmnpasayeva@gmail.com>
2022-07-14 13:47:48 +0400

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 let ply1 = Math.floor(Math.random() * 10);
 let ply2 = Math.floor(Math.random() * 10);
 
-function Click1(){
+function Click(){
     document.getElementById("screen1").innerText= ply1;
-}
-
-function Click2(){
     document.getElementById("screen2").innerText= ply2;
-}
-
-function Winner(){
     if (ply1 > ply2){
         document.getElementById("winner").innerText=("The WINNER is Player1!")
     } else if (ply2 >ply1){
